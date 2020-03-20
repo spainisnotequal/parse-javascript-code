c2a80df80dba563785af47e0bb045d448aa1425e
spainisnotequal <spainisnotequal@gmail.com>
2020-03-20 20:56:17 +0100

    Create the connection to the MongoDB database

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
+// In develpment, load environment variables (for instance, DEV_DB_URL)
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// Express JSON parser
+app.use(express.json());
+
+// MongoDB configuration
+const mongoDB = process.env.MONGODB_URI || process.env.DEV_DB_URL;
+
+// Connect to the database
+mongoose.connect(mongoDB,{ useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch(error => console.log("MongoDB connection error: ", error));
+
+// Configurte port
 const PORT = process.env.PORT || 5000;
 
-// console.log that your server is up and running
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+// Start the server and console.log that the server is up and running
+app.listen(PORT, () => console.log(`Server started (listening on port ${PORT})`));
