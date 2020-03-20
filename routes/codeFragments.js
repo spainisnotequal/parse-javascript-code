6d9c7d7630b556d65f685431eaac1213acbb1018
spainisnotequal <spainisnotequal@gmail.com>
2020-03-20 23:45:34 +0100

    Create the POST api/codeFragments route

--- routes/codeFragments.js
+++ routes/codeFragments.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const parseGithubURL = require('parse-github-url');
+const acorn = require("acorn");
 
 // CodeFragment Model
 const CodeFragment = require('../models/CodeFragment');
@@ -11,6 +13,24 @@ router.get('/', (req, res) => {
   CodeFragment.find()
     .sort({ date: -1})
     .then(fragments => res.json(fragments))
+});
+
+// @route        POST api/codeFragments
+// @description  Create a code fragment
+// @acces        Public
+router.post('/', (req, res) => {
+  const { source, code } = req.body;
+  const { owner } = parseGithubURL(source);
+  console.log(owner);
+  const ast = acorn.parse(code);
+  const newCodeFragment = new CodeFragment({
+    author: owner,
+    source,
+    code,
+    ast
+  });
+
+  newCodeFragment.save().then(codeFragment => res.json(codeFragment));
 });
 
 
