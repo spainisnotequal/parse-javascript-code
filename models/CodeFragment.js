c62f21bde78ff75165267ebf7cd4b876698982b3
spainisnotequal <spainisnotequal@gmail.com>
2020-03-20 22:04:34 +0100

    Create the database model (we'll only have one model)

---
+++ models/CodeFragment.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const CodeFragmentSchema = new Schema({
+  author: {
+    type: String,
+    max: 100
+  },
+  source: {
+    type: String,
+    max: 200
+  },
+  code: {
+    type: String,
+    required: true
+  },
+  ast: {
+    type: String
+  }
+});
+
+// Export the model to be able to import it in other files
+module.export = CodeFragment = mongoose.model('codeFragment', CodeFragmentSchema);
