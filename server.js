88f8d4cb6cfcc4a761a8a90fdc09d1be15ccbb62
spainisnotequal <spainisnotequal@gmail.com>
2020-03-22 14:57:47 +0100

    Prepare the app to deploy it to Heroku

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 // In development, load environment variables (for instance, DEV_DB_URL)
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
 // Import modules
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 
 // Import routes
-const codeFragments = require('./routes/codeFragments');
+const codeFragments = require("./routes/codeFragments");
 
 // Create the Express app
 const app = express();
@@ -20,15 +21,28 @@ app.use(express.json());
 const mongoDB = process.env.MONGODB_URI || process.env.DEV_DB_URL;
 
 // Connect to the database
-mongoose.connect(mongoDB,{ useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
   .catch(error => console.log("MongoDB connection error: ", error));
 
 // Use Routes
-app.use('/api/codeFragments', codeFragments);
+app.use("/api/codeFragments", codeFragments);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Configurte port
 const PORT = process.env.PORT || 5000;
 
 // Start the server and console.log that the server is up and running
-app.listen(PORT, () => console.log(`Server started (listening on port ${PORT})`));
+app.listen(PORT, () =>
+  console.log(`Server started (listening on port ${PORT})`)
+);
