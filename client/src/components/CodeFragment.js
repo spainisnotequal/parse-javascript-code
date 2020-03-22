eb0e34de6e5204ae88000b7c138531749889b676
spainisnotequal <spainisnotequal@gmail.com>
2020-03-22 14:23:13 +0100

    Connect the frontend to the backend

--- client/src/components/CodeFragment.js
+++ client/src/components/CodeFragment.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import React from "react";
+import { Col, Row, Form, FormGroup, Label, Input } from "reactstrap";
 
 const CodeFragment = props => {
-  const {author, source, code, ast} = props;
+  const { author, source, code, ast } = props;
   return (
     <Form>
       <Row form>
@@ -21,21 +21,12 @@ const CodeFragment = props => {
       </Row>
       <FormGroup>
         <Label>JavaScript code</Label>
-        <Input type="text" value={code}/>
+        <Input type="text" value={code} />
       </FormGroup>
       <FormGroup>
         <Label>Abstract Syntax Tree</Label>
-        <Input type="text" value={ast}/>
+        <Input type="text" value={ast} />
       </FormGroup>
-      <Button
-        className="remove-btn"
-        color="secondary"
-        size="sm"
-        style={{ float: 'right', marginBotton: '2rem' }}
-        onClick={() => console.log("Delete Code Fragment (to be implemented...)")}
-        >
-        Delete
-      </Button>
     </Form>
   );
 };
